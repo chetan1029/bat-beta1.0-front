31e40fa3ddfacf68da357bbac856742d0bcaed3e
Chetan <chetanbadgujar92@gmail.com>
2021-01-28 11:21:43 +0100

    component packing box from Duong

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     showErrorStack: true,
-    // API_URL: "http://localhost:8000",
-    API_URL: "https://api.thebatonline.com/",
+    API_URL: "http://localhost:8000",
+    //API_URL: "https://api.thebatonline.com/",
 };
 
 export default config;
